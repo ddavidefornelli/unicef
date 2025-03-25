c26ea05bddb98653bda6cd38d1973e3fde9f1c71
ddavide.fornelli <illernofedivad05@gmail.com>
2025-03-25 16:28:45 +0100

    ho cambiato un po di cose

--- ciao.c
+++ ciao.c
@@ -1,12 +1,4 @@
+
 int main() {
     return 0;
-}
-
-void ao(){
-    printf("affammoccA");
-    printf("Il gruppo di francesco viene bocciato");
-}
-
-void ciaobelli() {
-  printf("affammoccAsort");
 }
