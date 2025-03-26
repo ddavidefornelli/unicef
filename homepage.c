acd98a910cb651b2ba0beb869a0e97e0130d2dbe
GiulianoUnicef <giulianoantoniciello54@gmail.com>
2025-03-26 21:58:48 +0100

    aggiunta commenti su homepage

--- homepage.c
+++ homepage.c
@@ -1,9 +1,28 @@
+/*
+Autori:
+Fornelli Davide
+Antoniciello Giuliano
+
+Data di inizio di scrittura del file: 
+26/03/2025
+
+Nome del file:
+Home Page sudoku
+
+Scopo di ogni funzione presente:
+stampaCentrato() -> funzione utilizzata per centrare sul terminale il menù di partenza;
+mostraMenu() -> funzione utilizzata per stampare a video il menù di partenza;
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define LARGHEZZA 120 /* serve per rappresentare la larghezza del terminale */
 
-void stampaCentrato(const char *TESTO);
-void mostraMenu();
+int main();
+void stampaCentrato(const char *testo); /* funzione per centrare sul terminale una stringa. Si passa come paramentro la stringa da centrare */
+void mostraMenu(); /* funzione per stampare a video il menù di partenza */
+
 
 int main() {
   mostraMenu();
@@ -11,27 +30,61 @@ int main() {
   return 0;
 }
 
-void stampaCentrato(const char *TESTO) {
-  const int LARGHEZZA = 120;
-  int len;
-  int spazi; 
-  int i;
 
-  len = strlen(TESTO);
-  spazi = (LARGHEZZA - len) / 2;
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+*  FUNZIONE: stampaCentrato(const char *testo)             *
+*  DESCRIZIONE: Allinea la stringa "testo"sul terminale    *
+*                                                          *
+*  ARGOMENTI:                                              *
+*  const char *testo: stringa da allineare                 *
+*                                                          *
+*  RITORNO: //                                             *
+*                                                          *
+*  MODIFICHE:                                              *
+*  2025/03/26 - Prima versione                             *
+*                                                          *
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * *  */
+void stampaCentrato(const char *testo) 
+{
+  /* Definisco ed inizializzo la variabile */
+  // int spazi = (LARGHEZZA - len) / 2;
+  int spaziDaInserire = 0; /* contatore per il ciclo */
+  int dimStringa = strlen(testo); /* calcolo la lunghezza di dimStringa */
 
-  if (spazi < 0) spazi = 0;
+  /* serve per centrare la stringa quando è stampato a video (il valore di "spazi" è adattato alle dimensioni di LARGHEZZA) */
+  int spazi = (LARGHEZZA - dimStringa) / 2;
 
-  i = 0;
-  while(i < spazi){
-    printf(" ");
-    i = i + 1;
+  /* Se "testo" è più lunga della larghezza del terminale, evito spazi negativi */
+  if (spazi < 0)
+  {
+    spazi = 0;
   }
-  printf("%s\n", TESTO);
+  
+  /* Stampo tanti spazi vuoti quanto basta per centrare "testo" */
+  while(spaziDaInserire < spazi)
+  {
+    printf(" ");
+    spaziDaInserire++;
+  }
+  printf("%s\n", testo);
 }
 
-void mostraMenu() {
-  int sceltaUtente;
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+*  FUNZIONE: mostraMenu()                                  *
+*  DESCRIZIONE: Mostra il menù iniziale del gioco          *
+*                                                          *
+*  ARGOMENTI: //                                           *
+*                                                          *
+*  RITORNO: //                                             *
+*                                                          *
+*  MODIFICHE:                                              *
+*  2025/03/26 - Prima versione                             *
+*                                                          *
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * *  */
+void mostraMenu() 
+{
+  //int sceltaUtente;
   printf("\n");
   stampaCentrato(" ");
   stampaCentrato(" __           _       _           ");
@@ -39,16 +92,15 @@ void mostraMenu() {
   stampaCentrato("\\ \\| | | |/ _` |/ _ \\| |/ / | | | ");
   stampaCentrato("_\\ \\ |_| | (_| | (_) |   <| |_| | ");
   stampaCentrato("\\__/\\__,_|\\__,_|\\___/|_|\\_\\\\__,_| ");
-  stampaCentrato(" ");
-  printf("\n");
-
+  printf("\n\n");
+  
   stampaCentrato("digita un numero per selezionare la modalita' di gioco e premi invio per confermare:");
   printf("\n");
+  
   stampaCentrato("1) Tutorial  ");
   stampaCentrato("2) Facile    ");
   stampaCentrato("3) Media     ");
   stampaCentrato("4) Difficile ");
-  /*scanf("%d", &sceltaUtente);*/
-  /*printf("%d", sceltaUtente);*/
+  //scanf("%d", &sceltaUtente);
+  //printf("%d", sceltaUtente);
 }
-
